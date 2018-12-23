32269a3569968805a7a58e1ad46a836eff0e3cea
Andrew Millar <duckman.andrew@gmail.com>
2018-12-24 00:38:00 +0100

    First commit
    Factionality is there

---
+++ dateNames.js
@@ -0,0 +1,3 @@
+const weekDays =['Sunday', 'Monday', 'Tuesday', 'Wendesday', 'Thursday', 'Friday', 'Saterday'];
+const months = ['January', 'February', 'March','April','May','June','July','August','September','October','November','December'];
+const postFix = ['st', 'nd', 'rd', 'th'];
---
+++ datefunction.js
@@ -0,0 +1,26 @@
+const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wendesday', 'Thursday', 'Friday', 'Saterday'];
+const months = ['January', 'February', 'March','April','May','June','July','August','September','October','November','December'];
+const postFix = ['st', 'nd', 'rd', 'th']
+
+
+function formatDate(dateStr) {
+    // Input in the form of a Epoch time stamp, a date string or comma seperated time idication
+    // Output in the form "Thursday, 18 May 1978"
+
+    const   date = new Date(dateStr),
+         options = { weekday: 'long', day: 'numeric', year: 'numeric', month: 'long' },
+    formatedDate = date.toLocaleDateString('en-NZ', options),// I like the New Zeeland variant
+           hours = date.getHours(),
+         minutes = date.getMinutes() < 10? `0${date.getMinutes()}`: date.getMinutes();
+
+    // For parsing on a webpage the colon needs to be replaced with the html entity
+    if(date.getDate() == new Date().getDate()){
+        return `Today, ${hours}&#58;${minutes}`;
+    } else if(date.getDate()+1 ===  new Date().getDate()) {
+        return `Yesterday, ${hours}&#58;${minutes}`;
+    } else if (date.getDate()+2 ===  new Date().getDate()) {
+        return `Two days ago, ${hours}&#58;${minutes}`;
+    }
+    return formatedDate;
+}
+
