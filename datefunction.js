02819466d886acaacaae107974bb8a493b9309bb
Andrew Millar <duckman.andrew@gmail.com>
2018-12-24 14:48:10 +0100

    Add a number of return options

--- datefunction.js
+++ datefunction.js
@@ -5,24 +5,38 @@ const postFix = ['st', 'nd', 'rd', 'th']
 
 function formatDate(dateStr) {
     // Input in the form of a Epoch time stamp, a date string or comma seperated time idication
-    // Output in the form "Thursday, 18 May 1978"
+    // Output in the form "Thursday, 18 May 1978" is date is more than 48 hours ago, else a different 
+    // time indication
 
     const   date = new Date(dateStr),
+             now = new Date(),
          options = { weekday: 'long', day: 'numeric', year: 'numeric', month: 'long' },
-    formatedDate = date.toLocaleDateString('en-NZ', options),// I like the New Zeeland variant
+    formatedDate = date.toLocaleDateString('en-NZ', options), // I like the New Zeeland variant
            hours = date.getHours(),
+          totMin = hours * 60 + date.getMinutes(), // Total number of minutes until date string
+       totNowMin = now.getHours()*60 + now.getMinutes(), // Total munber of minutes until now
          minutes = date.getMinutes() < 10? `0${date.getMinutes()}`: date.getMinutes();
 
-    // For parsing on a webpage the colon needs to be replaced with the html entity
-    // TODO: add the 'just now', 'half an hour ago', 'an hour ago', 'a few hours ago', 'earlier today' 
-    //  0-20min, 20-50min, 50min-110min, check if it was the same day
-    if(date.getDate() == new Date().getDate()){
-        return `Today, ${hours}&#58;${minutes}`;
-    } else if(date.getDate()+1 ===  new Date().getDate()) {
+    //Not pretty but a switch will not work
+    if(date.getDate() == now.getDate() && (totNowMin - totMin) < 20) {
+        console.log((totNowMin - totMin));
+        return "Just now";
+    } else if (date.getDate() == now.getDate() && (totNowMin - totMin) < 50) {
+        console.log((totNowMin - totMin));
+        return "About half an hour ago";
+    } else if (date.getDate() == now.getDate() && (totNowMin - totMin) < 110) {
+        console.log((totNowMin - totMin));
+        return "About an hour ago";
+    } else if (date.getDate() == now.getDate() && (totNowMin - totMin) < 150) {
+        console.log((totNowMin - totMin));
+        return "A few hours ago";
+    } else if (date.getDate() == now.getDate() && (totNowMin - totMin) > 150) {
+        console.log((totNowMin - totMin));
+        return "Earlier today";
+    } else if(date.getDate()+1 ===  now.getDate()) {
         return `Yesterday, ${hours}&#58;${minutes}`;
-    } else if (date.getDate()+2 ===  new Date().getDate()) {
+    } else if (date.getDate()+2 ===  now.getDate()) {
         return `Two days ago, ${hours}&#58;${minutes}`;
     }
     return formatedDate;
 }
-
